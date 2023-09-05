ca4eb4336ebe9e6605a81a98b2a6c5147f986946
FedericaCampo97 <federica.campo97@gmail.com>
2023-09-05 22:20:35 +0200

    exercise complete

--- assets/main.js
+++ assets/main.js
@@ -4,36 +4,40 @@ Ogni secondo il nostro countdown dovrà scalare fino alle 9:30
 di domani mattina!*/
 
 
-//data odierna
-const now = new Date()
-console.log(now)
+
 //data dell'evento
 let eventDate = new Date('09/06/2023 09:30:00');
 console.log(eventDate)
-//ricavo i millisecondi da adesso all'evento
-let differtsTime = eventDate - now
-console.log(differtsTime)
-//trasformo i millisecondi in secondi, minuti e ore
-let seconds = differtsTime / 1000
-let minuts = seconds / 60
-let hours = minuts /  60
-
-let hoursCountdown = Math.floor(hours);
-console.log(hoursCountdown);
-let minutsDif = (hours - hoursCountdown) * 60;
-console.log(minutsDif);
-let minutsCountdown = Math.floor(minutsDif);
-console.log(minutsCountdown);
-let secondsDif = (minutsDif - minutsCountdown) * 60;
-console.log(secondsDif);
-let secondsCountdown = Math.floor(secondsDif);
-console.log(minutsCountdown)
-
-console.log (hoursCountdown + ':' + minutsCountdown + ':' + secondsCountdown)
 
 //Metto in funzione il countdown 
 setInterval (countdown , 1000); 
 function countdown() {
+    //data odierna
+    const now = new Date()
     
+    //ricavo i millisecondi da adesso all'evento
+    let differtsTime = eventDate - now
+    
+    //trasformo i millisecondi in secondi, minuti e ore
+    let seconds = differtsTime / 1000
+    let minuts = seconds / 60
+    let hours = minuts /  60
+
+    let hoursCountdown = Math.floor(hours);
+    
+    let minutsDif = (hours - hoursCountdown) * 60;
+
+    
+    let minutsCountdown = Math.floor(minutsDif);
+    
+    let secondsDif = (minutsDif - minutsCountdown) * 60;
+    
+    let secondsCountdown = Math.floor(secondsDif);
+    
+    console.log (
+        
+        String("0" + hoursCountdown).slice(-2) + ':' + 
+        String("0" + minutsCountdown).slice(-2) + ':' + 
+        String("0" + secondsCountdown).slice(-2))
 }
 
