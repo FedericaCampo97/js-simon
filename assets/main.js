0ffc85209ca2ef3d844d2b0bc8d436081d28b428
FedericaCampo97 <federica.campo97@gmail.com>
2023-09-05 15:19:42 +0200

    I set the date of the event to one day before

--- assets/main.js
+++ assets/main.js
@@ -3,3 +3,19 @@ per la correzione di domani!
 Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 
 di domani mattina!*/
 
+let interval;
+
+//Imposto la data di scadenza
+const correctionDay = new Date('06/09/2023');
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+
+
+correctionDay = new Date(correctionDay.getTime() -24 * hour * minute * second);
+
+
+
