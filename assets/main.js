3c04fe0de27d45417b28074d04adc2ad006acd8d
FedericaCampo97 <federica.campo97@gmail.com>
2023-09-05 21:13:59 +0200

    restart exercise from zero

--- assets/main.js
+++ assets/main.js
@@ -3,19 +3,10 @@ per la correzione di domani!
 Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 
 di domani mattina!*/
 
-let interval;
 
-//Imposto la data di scadenza
-const correctionDay = new Date('06/09/2023');
-
-const second = 1000;
-const minute = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
-
-
-
-correctionDay = new Date(correctionDay.getTime() -24 * hour * minute * second);
-
-
+//data odierna
+//data dell'evento
+//ricavo i millisecondi da adesso all'evento
+//trasformo i millisecondi in secondi, minuti e ore
+//stampo in console il risultato ogni secondo
 
